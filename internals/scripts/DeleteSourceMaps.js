61a4b0c616314881c8529148399f49185b9d398a
Nguyen Hieu Liem <hieuliemfe@gmail.com>
2020-12-06 17:10:57 +0700

    change logo

--- internals/scripts/DeleteSourceMaps.js
+++ internals/scripts/DeleteSourceMaps.js
@@ -1,27 +1,27 @@
 import path from 'path';
 import rimraf from 'rimraf';
-import fs from 'fs';
+// import fs from 'fs';
 
 export default function deleteSourceMaps() {
-  // rimraf.sync(path.join(__dirname, '../../app/dist/*.js.map'));
-  // rimraf.sync(path.join(__dirname, '../../app/*.js.map'));
-  const srcMapExist = fs.existsSync(
-    path.join(__dirname, '../../app/renderer.prod.js.map')
-  );
-  if (srcMapExist) {
-    rimraf.sync(path.join(__dirname, '../../app/renderer.prod.js.map'));
-  }
-  const distSrcMapExist = fs.existsSync(
-    path.join(__dirname, '../../app/dist/renderer.prod.js.map')
-  );
-  if (distSrcMapExist) {
-    fs.copyFile(
-      path.join(__dirname, '../../app/dist/renderer.prod.js.map'),
-      path.join(__dirname, '../../app/renderer.prod.js.map'),
-      (err) => {
-        if (err) throw err;
-        console.log('Source map added successfully!');
-      }
-    );
-  }
+  rimraf.sync(path.join(__dirname, '../../app/dist/*.js.map'));
+  rimraf.sync(path.join(__dirname, '../../app/*.js.map'));
+  // const srcMapExist = fs.existsSync(
+  //   path.join(__dirname, '../../app/renderer.prod.js.map')
+  // );
+  // if (srcMapExist) {
+  //   rimraf.sync(path.join(__dirname, '../../app/renderer.prod.js.map'));
+  // }
+  // const distSrcMapExist = fs.existsSync(
+  //   path.join(__dirname, '../../app/dist/renderer.prod.js.map')
+  // );
+  // if (distSrcMapExist) {
+  //   fs.copyFile(
+  //     path.join(__dirname, '../../app/dist/renderer.prod.js.map'),
+  //     path.join(__dirname, '../../app/renderer.prod.js.map'),
+  //     (err) => {
+  //       if (err) throw err;
+  //       console.log('Source map added successfully!');
+  //     }
+  //   );
+  // }
 }
